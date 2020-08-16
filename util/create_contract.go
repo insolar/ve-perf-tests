300f32e2014db4d6d049da0faec6c72884eb4ee4
Sergey Kudasov <sergey.kudasov@insolar.io>
2020-08-16 11:49:51 +0300

    sleep

--- util/create_contract.go
+++ util/create_contract.go
@@ -2,16 +2,18 @@ package util
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/insolar/loaderbot"
 )
 
 func CreateWallets(url string, amount int) ([]string, error) {
 	fmt.Printf("Generating %d wallets\n", amount)
-	client := loaderbot.NewLoggingHTTPClient(true, 30)
+	client := loaderbot.NewLoggingHTTPClient(false, 30)
 	wallets := make([]string, 0)
 	for i := 0; i < amount; i++ {
 		fmt.Printf("creating wallet #%d\n", i)
+		time.Sleep(100 * time.Millisecond)
 		ref, err := CreateSimpleWallet(client, url+"/wallet/create")
 		if err != nil {
 			return nil, err
