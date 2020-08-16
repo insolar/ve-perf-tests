dca8dcb6198cdd05086b9ebccda278fb579ab6e2
Sergey Kudasov <sergey.kudasov@insolar.io>
2020-08-16 12:50:50 +0300

    update dep, raise timeout to 60s

--- util/create_contract.go
+++ util/create_contract.go
@@ -2,18 +2,16 @@ package util
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/insolar/loaderbot"
 )
 
 func CreateWallets(url string, amount int) ([]string, error) {
 	fmt.Printf("Generating %d wallets\n", amount)
-	client := loaderbot.NewLoggingHTTPClient(false, 30)
+	client := loaderbot.NewLoggingHTTPClient(false, 60)
 	wallets := make([]string, 0)
 	for i := 0; i < amount; i++ {
 		fmt.Printf("creating wallet #%d\n", i)
-		time.Sleep(100 * time.Millisecond)
 		ref, err := CreateSimpleWallet(client, url+"/wallet/create")
 		if err != nil {
 			return nil, err
